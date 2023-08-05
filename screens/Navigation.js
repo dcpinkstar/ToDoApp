dad7ce71e246ce51551063f6b3725a565a45c0f8
Casey Hatcher <casey.willetts@gmail.com>
2023-08-05 16:26:40 -0400

    daily etc screens

--- screens/Navigation.js
+++ screens/Navigation.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
 import HomeScreen from './HomeScreen';
+import OnceScreen from './OnceScreen';
+import DailyScreen from './DailyScreen';
+import WeeklyScreen from './WeeklyScreen';
+import MonthlyScreen from './MonthlyScreen';
 
 const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+const FrequencyStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='Once' component={OnceScreen} />
+      <Stack.Screen name='Daily' component={DailyScreen} />
+      <Stack.Screen name='Weekly' component={WeeklyScreen} />
+      <Stack.Screen name='Monthly' component={MonthlyScreen} />
+    </Stack.Navigator>
+  )
+}
 
 const Navigation = () => {
   return (
